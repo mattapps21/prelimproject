03eaf630f7c7833b553caabe77f31468da8fc093
mattX <skywave.x21@gmail.com>
2017-01-02 18:52:14 +0800

    fifth commit input validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,11 @@ class UserController extends Controllers
 	
 	public function postSignUp(Request $request)
 	{
+		$this=>validate($request,[
+		'email' => 'required|mail|unique:users',
+		'first_name' => 'required|max:120',
+		'password' =>'required|min:4'
+		]);
 		$email = $request['email'];
 		$first_name  = $request['first_name'];
 		$password = bcrypt($request['password']);
@@ -30,6 +35,11 @@ class UserController extends Controllers
 	
 		public function postSignIn(Request $request)
 	{
+		$this=>validate($request,[
+		'email' => 'required|',
+		'password' =>'required|'
+		]);
+		
 		if (Auth::attempt(['email' = > $request['email'], 'password' = $request['password'])) {
 			return redirect () -> route ('dashboard');
 			
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,27 +5,40 @@ Welcome
 @endsection
 
 @section('content')
+
+@if(count($errors) > 0)
+
+<div class = "row"> 
+<div class = "col-md-4 col-md-offset-4"> 
+<ul> 
+@foreach($errors =>all as $error)
+<li> ($error)</li> @endforeach
+</ul>
+</div>
+</div>
+
+
 <div class = "row"> 
 <div class = "col-md-6"> 
 <h3> Sign Up </h3>
 <form action ="#" method = "post"> 
-<div class = "form-group"> 
+<div class = "form-group" {{$errors->'has('email') ? 'has-error : ''}}> 
 <label for="email"> Your E-mail</label>
-<input class = "form-control" type = "text" name = "email" id = "email">
+<input class = "form-control " type = "text" name = "email" id = "email" value = "{{_Request::old('email'))}}">
 </div>
 
-<div class = "form-group"> 
+<div class = "form-group" {{$errors->'has('first_name') ? 'has-error : '''}}> 
 <label for="first_name"> Your First Name</label>
-<input class = "form-control" type = "text" name = "first_name" id = "first_name">
+<input class = "form-control" type = "text" name = "first_name" id = "first_name" value = "{{_Request::old('first_name'))}}">
 </div>
 
-<div class = "form-group"> 
+<div class = "form-group" {{$errors->'has('password') ? 'has-error : '''}}> 
 <label for="email"> Your Password</label>
-<input class = "form-control" type = "password" name = "password" id = "password">
+<input class = "form-control" type = "password" name = "password" id = "password" value = "{{_Request::old('password'))}}">
 </div>
 
 <button type = "submit" class = "btn btn-primary"> Submit </button>
-<input type = "hidden" name = "_token" value = "{{Session:: token ()}}"
+<input type = "hidden" name = "_token" value = "{{Session:: token ()}}">
 </form>
 
 
@@ -37,16 +50,16 @@ Welcome
 <h3> Sign In </h3>
 <form action ="{{route {'signin '}}}" method = "post"> 
 <div class = "form-group"> 
-<label for="email"> Your E-mail</label>
-<input class = "form-control" type = "text" name = "email" id = "email">
+<label for="email"  {{$errors->'has('email') ? 'has-error : ''}}> Your E-mail</label>
+<input class = "form-control" type = "text" name = "email" id = "email" value = "{{_Request::old('email'))}}">
 </div>
 <div class = "form-group"> 
-<label for="email"> Your Password</label>
-<input class = "form-control" type = "password" name = "password" id = "password">
+<label for="email"  {{$errors->'has('password') ? 'has-error : ''}}> Your Password</label>
+<input class = "form-control" type = "password" name = "password" id = "password" value = "{{_Request::old('password'))}}">>
 </div>
 
 <button type = "submit" class = "btn btn-primary"> Submit </button>
-<input type = "hidden" name = "_token" value = "{{Session:: token ()}}"
+<input type = "hidden" name = "_token" value = "{{Session:: token ()}}">
 </form>
 
 
